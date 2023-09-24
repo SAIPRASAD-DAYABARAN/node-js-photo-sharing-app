23de794a5829f958eefd14ff577dbe381df5a586
Sai Prasad <42207241+SAIPRASAD-DAYABARAN@users.noreply.github.com>
2023-09-24 19:00:36 +0530

    Update example_server.js
    if/else statements

--- example_server.js
+++ example_server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const server = http.createServer(
     (request, response) =>{
-        console.log(`Server running at http://localhost:8000`);
-        console.log(request.url);
-        response.end('Hello kinsta');
+        if (request.url == '/about')
+            response.end( 'The about page');
+        else if( request.url == '/login')
+            response.end( 'Logged in!');
+        else if( request.url == '/logout')
+            response.end( 'Logged out!');
+        else {
+            response.writeHead(404)
+            response.end('Page Not Found')
+        }
     }
-)
+);
 server.listen(8080)
 console.log("Server running on http://localhost:8080")
+//adding this at the end coz otherwise we would see a blank line and not "listening.." and it would look like its hanging
