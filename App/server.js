9c80e10347687feb415520fa9a30f4710ca9a712
Sai Prasad <42207241+SAIPRASAD-DAYABARAN@users.noreply.github.com>
2023-09-25 17:37:17 +0530

    public folder
    setting up public folder for serving static files

--- App/server.js
+++ App/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = new express();
 const port = 8080;
-
+app.use(express.static('public')) // this line will enable everything in /public to be served up as a static file
 app.listen(port, ()=>{
     console.log(`Serving photo app on http://localhost:${port}`);
-})
+}) 
