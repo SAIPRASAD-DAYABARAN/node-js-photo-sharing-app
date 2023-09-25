21af83152b65f241a1466b7bedf1c88ae4e07d4a
Sai Prasad <42207241+SAIPRASAD-DAYABARAN@users.noreply.github.com>
2023-09-25 20:00:21 +0530

    Update server.js
    set view engine to EJS

--- App/server.js
+++ App/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = new express();
 const port = 8080;
 app.use(express.static('public')) // this line will enable everything in /public to be served up as a static file
+app.set("view engine", "ejs");
 app.listen(port, ()=>{
     console.log(`Serving photo app on http://localhost:${port}`);
 }) 
+//we should create a GET request route for the whole path
+app.get("/", (request, response)=>{
+    response.render("index"); //res.render will look in a views folder the view
+})
