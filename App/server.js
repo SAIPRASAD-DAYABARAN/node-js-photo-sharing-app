00a4cb46b67f7ffafdaef85ff87102bf5cd8a9d5
Sai Prasad <42207241+SAIPRASAD-DAYABARAN@users.noreply.github.com>
2023-09-26 15:26:54 +0530

    Update server.js
    add sequelize connection logic

--- App/server.js
+++ App/server.js
@@ -4,19 +4,27 @@ const db = require('./models');
 //we only have to say ./models because pulling from index.js, and index.js is defining the model i.e., our database
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+app.use(bodyParser.json()) //we've added bodyParser with the ability to parse json
 app.use(express.static('public')) // this line will enable everything in /public to be served up as a static file
 app.set("view engine", "ejs");
 
 
 //db
 //connecting to our db
-const sqlPort = 3307; //3306 or 3307 by default
-app.listen(sqlPort, ()=>{
-    console.log(
-        `Mariadb Connection Successful - http://localhost:${sqlPort}.`
-    )
-})
+const sqlPort = 3306; //3306 or 3307 by default
+db.sequelize
+    .sync({}) //sync connects our db and makes it update
+    .then(()=>{ //passing an anonymous arrow function to then
+        app.listen(sqlPort, ()=>{
+            console.log(
+                `Mariadb Connection Successful - http://localhost:${sqlPort}.`
+            );
+        });
+        
+    })
+    .catch((error) =>{ //callback
+        console.error("Unable to connect to the db", error)
+    });
 
 //server
 const port = 8080;
